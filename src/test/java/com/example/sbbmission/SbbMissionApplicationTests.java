e54372aa6f90a0b602c7eaf3836af770a9b0e767
jyh1108 <kobcom12@naver.com>
2023-11-29 09:44:13 +0900

    3-6 30%

--- src/test/java/com/example/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/example/sbbmission/SbbMissionApplicationTests.java
@@ -1,37 +1,21 @@
 package com.example.sbbmission;
 
-import com.example.sbbmission.answer.Answer;
-import com.example.sbbmission.answer.AnswerRepository;
-import com.example.sbbmission.question.Question;
-import com.example.sbbmission.question.QuestionRepository;
+import com.example.sbbmission.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbMissionApplicationTests {
     @Autowired
-    private QuestionRepository questionRepository;
+    private QuestionService questionService;
 
-    @Autowired
-    private AnswerRepository answerRepository;
-    @Transactional
     @Test
-    void contextLoads() {
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        List<Answer> answerList = q.getAnswerList();
-
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+    void testJpa() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
     }
 }
