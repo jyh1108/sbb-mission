3c216c63ee4a9825f104bbbfcb74ec7f5d69644f
jyh1108 <kobcom12@naver.com>
2023-11-30 01:59:03 +0900

    3-10

--- src/main/java/com/example/sbbmission/answer/AnswerService.java
+++ src/main/java/com/example/sbbmission/answer/AnswerService.java
@@ -1,12 +1,14 @@
 package com.example.sbbmission.answer;
 
 
+import com.example.sbbmission.DataNotFoundException;
 import com.example.sbbmission.question.Question;
 import com.example.sbbmission.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +24,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
